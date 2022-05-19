aedff25adda35ab52ead8f50db08ab0409180417
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-19 19:45:14 +0200

    Include Devise and add basic test for homepage

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:kitty)
+  end
+
+
   test "should get home" do
     get pages_home_url
     assert_response :success
