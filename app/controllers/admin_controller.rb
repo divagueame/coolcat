2a46309134c40346c9085f48012f5a2ef0ef5186
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-21 20:23:17 +0200

    Add Flash to partial and tests

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,22 @@
 class AdminController < ApplicationController
-    before_action :authenticate_user!
+before_action :admin_filter
+        
 
-    before_action do 
-        redirect_to :root unless is_admin?
-    end
+def index
+end
 
 
 
-    def index
-    end
+    private
+
+    def admin_filter
+        if is_admin?
+            flash[:notice] = 'Admin Mode Activated 01101101011011001001110110110010111011101100100101'
+        else
+            flash[:alert] = 'No chiki chiki please...'
+            redirect_to :root
+        end
+    end 
+            
   end
   
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -9,14 +9,16 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     get admin_path
     assert_response :success
     assert_select '#admin-panel', 1
+    assert_equal flash[:notice], 'Admin Mode Activated 01101101011011001001110110110010111011101100100101'
   end
-      
+  
   test 'non admin should redirect to pages home' do
     sign_out users(:admin)
     sign_in users(:kitty)
     get admin_path
     assert_response :redirect
     assert_select '#admin-panel', 0
+    assert_equal flash[:alert], 'No chiki chiki please...'
   end
 
 end
