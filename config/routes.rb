f012344d6c868591c9ed8f8c896c361d8942dffe
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-01 11:28:29 +0200

    Edit Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'profile/index'
   resources :cats
-  root 'pages#home' 
+  root 'pages#home'
   get 'pages/home'
   devise_for :users
   get '/profile', to: 'profile#index'
