a39f6776cdd993d0d27bf861720f9e6ef76055e4
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-14 23:00:07 +0200

    Add test to update cat on profile page

--- test/system/cats_test.rb
+++ test/system/cats_test.rb
@@ -14,22 +14,30 @@ class CatsTest < ApplicationSystemTestCase
   test "should create cat" do
     visit cats_url
     click_on "New cat"
-
+    
     click_on "Create Cat"
-
+  
     assert_text "Cat was successfully created"
     click_on "Back"
   end
-
-
-  # test "should update Cat" do
-  #   click_on "Edit this cat", match: :first
-
-  #   click_on "Update Cat"
-
-  #   assert_text "Cat was successfully updated"
-  #   click_on "Back"
-  # end
+  
+  
+  test "should update Cat in the user profile" do
+    visit profile_index_path
+    initial_name = @cat.name
+    assert_text initial_name
+    
+    within id: dom_id(@cat) do
+      page.find(:css, 'a > img.action_btn').click
+    end
+    
+    fill_in 'cat_name', with: 'An updated kitty name'
+      click_on "Update Cat"
+    
+    assert_text 'An updated kitty name'
+    assert_no_text initial_name
+    
+  end
 
   test "should destroy Cat" do
     visit cat_url(@cat)
