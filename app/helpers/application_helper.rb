3670a23bbaf9399632cedc5438d5d5f72f00b07f
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-19 18:28:17 +0200

    Refactor Avatar and add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
-
-    def is_admin?
-        current_user && current_user.admin?
+  def is_admin?
+    current_user && current_user.admin?
+  end
+  
+  def avatar
+    if current_user.avatar.present?
+        image_tag current_user.avatar.variant(:thumb), class: 'header_avatar rounded-full inline'
+    else
+        image_tag 'default_avatar.jpg', class: 'header_avatar rounded-full inline'
     end
+  end
+
+  def avatar_link
+      link_to profile_index_path, class: 'user_profile_link' do
+        avatar
+      end
+  end
 end
