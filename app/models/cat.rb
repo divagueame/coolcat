86b944d70288776e37a52d3ecb44af55aefe4dae
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-06 23:21:49 +0200

    Fixed test and image size

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,11 +2,8 @@ class Cat < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-
   has_many_attached :images do |image|
-    # image.variant :basic, resize_to_limit: [nil, 300]
-    image.variant :basic, resize_to_fill: [400, 500]
+    # image.variant :basic, resize_to_limit: [150, 150]
+    image.variant :basic, resize_to_fill: [150, nil]
   end
-  
-
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,16 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     sign_in users(:kitty)
   end
 
-
-  test "should get home" do
+  test 'should get home' do
     get pages_home_url
     assert_response :success
   end
-  
+
   test "Logged out users don't show username" do
     sign_out users(:kitty)
     get pages_home_url
@@ -18,33 +17,30 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_select '#user_email', 0
   end
 
-  test "header partial should be present in root" do
+  test 'header partial should be present in root' do
     get root_path
 
     assert_response :success
     assert_select '#header_partial', 1
   end
-  
 
-  test "Users without avatar should not load avatar image" do
+  test 'Users without avatar should  load default avatar image' do
     get root_path
     assert_response :success
 
     assert_select '#header_partial', 1
-    assert_select 'img.header_avatar', 0
+    assert_select 'img.header_avatar', 1
+    assert_select 'img.header_default_avatar', 1
   end
 
-
-  test "if logged in and user has avatar, it should load on header partial" do
+  test 'if logged in and user has avatar, it should load on header partial' do
     sign_out users(:kitty)
     sign_in users(:mike)
-    
+
     get root_path
     assert_response :success
-    
+
     assert_select '#header_partial', 1
     assert_select 'img.header_avatar', 1
   end
-  
-
 end
